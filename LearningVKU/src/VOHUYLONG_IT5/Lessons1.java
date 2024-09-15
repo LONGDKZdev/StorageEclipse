a282835122aec6f2fdfc990b3563875371a27b2c
LONGDKZdev <longvo2549@gmail.com>
2024-09-15 20:02:33 +0700

    init

---
+++ LearningVKU/src/VOHUYLONG_IT5/Lessons1.java
@@ -0,0 +1,15 @@
+package VOHUYLONG_IT5;
+
+public class Lessons1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("Võ Huy Long");
+		int value1 = 418;
+		double value2 =8.5;
+		System.out.println("value1 = "+value1);
+		System.out.println("valueB = "+value2);
+		int valueA, valueB ;
+		System.out.println("valueA + valueB ="+valueA);
+	}
+}
