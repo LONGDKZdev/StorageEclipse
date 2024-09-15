d023d2b64508105a77e63cd85cf35f15674f4d98
LONGDKZdev <longvo2549@gmail.com>
2024-09-15 20:18:34 +0700

    init

--- LearningVKU/src/VOHUYLONG_IT5/Lessons1.java
+++ LearningVKU/src/VOHUYLONG_IT5/Lessons1.java
@@ -3,13 +3,27 @@ package VOHUYLONG_IT5;
 public class Lessons1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		System.out.println("Võ Huy Long");
-		int value1 = 418;
-		double value2 =8.5;
-		System.out.println("value1 = "+value1);
-		System.out.println("valueB = "+value2);
+		
+//////////////////////////////////////////////////
+	import java.util.Scanner;//lệnh để cho phép nhập giá trị từ bàn phím (chèn gói)
+	// create an object of Scanner
+	Scanner input = new Scanner(System.in);
+	// take input from the user
+	int number = input.nextInt();
+/////////////////////////////////////////////////		
+	System.out.println("Hello world!");
+	System.out.println("Võ Huy Long");
+	int value1 = 418;
+	double value2 =8.5;
+	System.out.println("value1 = "+value1);
+	System.out.println("valueB = "+value2);
+		
+////////nhập dữ liệu 
+////////import java.util.Scanner;
+//////// trực tiếp :VD double value1 = input.nextdouble();
+//////// gián tiếp 
 		int valueA, valueB ;
+		
 		System.out.println("valueA + valueB ="+valueA);
 	}
 }
