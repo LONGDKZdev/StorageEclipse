062b720f174192d4a1cc91cef8994a81bc92b413
LONGDKZdev <longvo2549@gmail.com>
2024-09-25 21:43:56 +0700

    init

---
+++ LearningVKU/src/Additional_Exercises_For_Homework/Grades_Table_Java.java
@@ -0,0 +1,16 @@
+package Additional_Exercises_For_Homework;
+import java.util.Scanner;
+public class Grades_Table_Java {
+
+	public static void main(String[] args) {
+		Scanner inValue = new Scanner (System.in);
+		
+		
+		
+		
+		
+		
+	inValue.close();	
+	}
+
+}
