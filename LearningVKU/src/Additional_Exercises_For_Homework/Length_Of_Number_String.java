da9b199a5bcf914313d337bf77c77a490df49c31
LONGDKZdev <longvo2549@gmail.com>
2024-09-28 20:19:20 +0700

    init

--- LearningVKU/src/Additional_Exercises_For_Homework/Length_Of_Number_String.java
+++ LearningVKU/src/Additional_Exercises_For_Homework/Length_Of_Number_String.java
@@ -5,15 +5,20 @@ public class Length_Of_Number_String {
 	public static void main(String[] args) {
 		int number = 0;
 		Scanner In = new Scanner(System.in);
-		System.out.println("Enter numner  = ");
-		int lenth = In.nextInt();
-		while( lenth / 10 != 0) {
-			number++;
-			lenth /= 10 ;
-			
-		}
-			
 		
+		System.out.print("Enter numner  = ");
+		int length = In.nextInt();
+		if(length < 0 ) {
+			while(length < 0) {
+				System.out.println("Enter again! = ");
+				length = In.nextInt();	
+			}
+		}
+			while( length != 0) {
+				number++;
+				length /= 10 ;
+			}	
+		System.out.println("Length of number string = "+number);
 	In.close();
 	}
 
